305f9f038cdb6b042fefd368eb4df73cfcf84f7b
mrman888 <khdmdcm@gmail.com>
2018-10-05 08:08:53 +0200

    k

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { LoadSpinnerComponent } from './components/load-spinner/load-spinner.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [NavigationComponent, LoadSpinnerComponent]
+})
+export class CoreModule { }
